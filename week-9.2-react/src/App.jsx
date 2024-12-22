e2411adb5e6721ef851150cea3d2c9ef1a1b17fe
Raj <yraj98391@gmail.com>
2024-12-22 13:49:17 +0530

    React Basics

---
+++ week-9.2-react/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+
+function App() {
+return(
+  <div>
+    <b>Hi there</b>
+    <Counter></Counter>
+  </div>
+)
+
+function Counter(){
+const [count , setCount] = useState(0);
+
+function increaseCount(){
+  setCount(count + 1);
+}
+
+function decreaseCount(){
+  setCount(count - 1);
+}
+
+function resetCount(){
+  setCount(0);
+}
+
+
+return <div>
+  <h1 id="text">{count}</h1>
+  
+  <button onClick={increaseCount}>Increase Count</button>
+  <button onClick={decreaseCount}>Decrease Count</button>
+  <button onDoubleClick={resetCount}>Reset Count</button>
+</div>
+
+
+}
+}
+export default App
